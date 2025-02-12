39862f58c8a6f430a9bb14f6f440c140efb1edc2
Khushboo Saleem <khushboosaleem25@gmail.com>
2025-02-12 12:01:04 +0500

    Add files via upload

---
+++ javascript/Assignment operator in js/temple literals.js
@@ -0,0 +1,15 @@
+// console.log("Hello world")
+
+
+// template Literal use for print some strings with variable in js
+
+let price = 10
+let quantity = 5
+// console.log("output is equal to: " + (price * quantity) + "Rupess.")
+
+// By Using Literals `` back ticks
+console.log(`The Output is equal to: ${price*quantity} Rupees.`)
+
+let a = 5
+let b = 6
+console.log(`a+b is equal to: ${a+b}`)
